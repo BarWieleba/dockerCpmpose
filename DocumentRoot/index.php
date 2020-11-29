cc6a83ca459c3c926d2b2afd0e1dcd6c7211d144
BarWieleba <barwieleba@gmail.com>
2020-11-29 13:47:56 +0100

    phpinfo

---
+++ DocumentRoot/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello</title>
+</head>
+<style>
+    .hello{
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: auto;
+        margin-bottom: auto;
+    }
+</style>
+<body>
+<h1 class="hello">Witaj</p>
+<?php
+	echo '<p>Hello world</p>';
+	phpinfo();
+?>
+
+</body>
+</html> 
